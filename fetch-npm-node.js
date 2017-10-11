1830355a43631805d5838f6f98997695aa86ffe4
toby ealden <toby.ealden@gmail.com>
2017-10-11 09:31:55 +0100

    apply patch to fix setting Promise libary on node

--- fetch-npm-node.js
+++ fetch-npm-node.js
@@ -1,15 +1,23 @@
 "use strict";
 
 var realFetch = require('node-fetch');
-module.exports = function(url, options) {
+
+var isomorphicFetch = function(url, options) {
 	if (/^\/\//.test(url)) {
 		url = 'https:' + url;
 	}
+
+	if (isomorphicFetch.Promise) {
+		realFetch.Promise = isomorphicFetch.Promise;
+	}
+
 	return realFetch.call(this, url, options);
 };
 
+module.exports = isomorphicFetch;
+
 if (!global.fetch) {
-	global.fetch = module.exports;
+	global.fetch = isomorphicFetch;
 	global.Response = realFetch.Response;
 	global.Headers = realFetch.Headers;
 	global.Request = realFetch.Request;
