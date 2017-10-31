9d19f9bb693647affd9a3d395afbf3d0f8560080
IGlautier <ivanglautier@gmail.com>
2017-10-31 14:08:48 +0000

    fix for react-native

--- fetch-npm-browserify.js
+++ fetch-npm-browserify.js
@@ -2,5 +2,8 @@
 // on the global object (window or self)
 //
 // Return that as the export for use in Webpack, Browserify etc.
+if (typeof global.self === "undefined") { // Self is not available by default in react-native
+  global.self = global;
+}
 require('whatwg-fetch');
 module.exports = self.fetch.bind(self);
